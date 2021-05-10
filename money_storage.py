9afdd04f72fe436b13af866ee52d9efe5e230798
Dawid Planeta <dawid0planeta@gmail.com>
2021-05-10 19:13:03 +0200

    Add simple MoneyStorage class

---
+++ money_storage.py
@@ -0,0 +1,12 @@
+from typing import List
+from money_unit import MoneyUnit
+
+class MoneyStorage:
+    def __init__(self, allowed_values: List[str], initial_units: List[MoneyUnit]):
+        self._money = {key: [] for key in allowed_values}
+        for unit in initial_units:
+            self._money[str(unit.value)].append(unit)
+
+    @property
+    def money(self):
+        return self._money
