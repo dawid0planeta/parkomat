9091e052767a817ed0c7d79f3530c6a9b6439f83
Dawid Planeta <dawid0planeta@gmail.com>
2021-05-20 15:12:21 +0200

    Provide a way to add money to MoneyStorage

--- core/errors.py
+++ core/errors.py
@@ -17,3 +17,8 @@ class ParkomatIncorrectTimeException(ParkomatException):
     '''
     Exception raised if value given to Clock is incorrect
     '''
+
+class ParkomatFullException(ParkomatException):
+    '''
+    Exception rasied if coin given to MoneyStorage won't fit
+    '''
--- core/money_storage.py
+++ core/money_storage.py
@@ -1,8 +1,11 @@
-from typing import List
+from core.errors import ParkomatFullException
+from typing import List, Set
+
 from core.money_unit import MoneyUnit
 
 class MoneyStorage:
-    def __init__(self, allowed_values: List[str], initial_units: List[MoneyUnit]):
+    _coins = {'0.01', '0.02', '0.05', '0.10', '0.20', '0.50', '1.00', '2.00', '5.00'}
+    def __init__(self, allowed_values: Set[str], initial_units: List[MoneyUnit]):
         self._money = {key: [] for key in allowed_values}
         for unit in initial_units:
             self._money[str(unit.value)].append(unit)
@@ -10,3 +13,13 @@ class MoneyStorage:
     @property
     def money(self):
         return self._money
+
+    def put_money(self, unit: MoneyUnit) -> None:
+        str_unit = str(unit)
+        if str_unit not in self._coins or len(self._money[str_unit]) < 200:
+            self._money[str_unit].append(unit)
+        else:
+            raise ParkomatFullException
+
+
+
