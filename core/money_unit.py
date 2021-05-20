32253b25a2e4e7570bb52a89eaef1c24afe718e0
Dawid Planeta <dawid0planeta@gmail.com>
2021-05-20 15:13:05 +0200

    Change list of allowed units to set in MoneyUnit

--- core/money_unit.py
+++ core/money_unit.py
@@ -7,7 +7,7 @@ class MoneyUnit:
     A class to represent units of money. Coins or banknotes
     '''
 
-    _allowed = ('0.01', '0.02', '0.05', '0.10', '0.20', '0.50', '1.00', '2.00', '5.00', '10.00', '20.00', '50.00')
+    _allowed = {'0.01', '0.02', '0.05', '0.10', '0.20', '0.50', '1.00', '2.00', '5.00', '10.00', '20.00', '50.00'}
 
     def __init__(self, value: str):
         if value in MoneyUnit._allowed:
@@ -21,6 +21,9 @@ class MoneyUnit:
     @property
     def value(self):
         return self._value
+
+    def __str__(self):
+        return str(self._value)
 
 
 
