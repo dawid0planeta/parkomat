02ca7f06d0d8ea324ae411f2c7171c8b39810b11
Dawid Planeta <dawid0planeta@gmail.com>
2021-05-22 19:22:54 +0200

    Add proper tests according to specification

--- tests.py
+++ tests.py
@@ -1,34 +1,141 @@
-from decimal import Decimal
+import unittest
+from datetime import datetime, date, timedelta, time
 
-from parkomat.core import MoneyUnit, MoneyStorage
-
-allowed = ('0.01', '0.02', '0.05', '0.10', '0.20', '0.50', '1.00', '2.00', '5.00', '10.00', '20.00', '50.00')
-
-def test_money_unit():
-    for each in allowed:
-        assert MoneyUnit(each).value == Decimal(each)
+from parkomat.core import MoneyUnit, Parkomat
+from parkomat.core.errors import *
 
 
-def test_money_storage():
-    test_money_str = ['0.01', '0.01', '0.50', '0.20', '0.20', '0.50', '1.00', '1.00', '5.00', '10.00', '20.00', '50.00']
-    test_money_objects = [MoneyUnit(unit) for unit in test_money_str]
-    storage = MoneyStorage(allowed, test_money_objects)
-    expected = {
-        '0.01': [test_money_objects[0], test_money_objects[1]],
-        '0.02': [],
-        '0.05': [],
-        '0.10': [],
-        '0.20': [test_money_objects[3], test_money_objects[4]],
-        '0.50': [test_money_objects[2], test_money_objects[5]],
-        '1.00': [test_money_objects[6], test_money_objects[7]],
-        '2.00': [],
-        '5.00': [test_money_objects[8]],
-        '10.00': [test_money_objects[9]],
-        '20.00': [test_money_objects[10]],
-        '50.00': [test_money_objects[11]],
-    }
+class TestParkomat(unittest.TestCase):
+    parkomat = Parkomat()
 
-    assert storage.money == expected
+    def test_inccorect_time_set(self):
+        '''
+        Test if Clock will throw an error given incorrect time
+        '''
+        self.parkomat.reset()
+        self.parkomat._clock._curr_time = datetime.combine(date(2021, 5, 24), datetime.now().time())
 
-test_money_unit()
-test_money_storage()
+        with self.assertRaises(ParkomatIncorrectTimeException):
+            self.parkomat.curr_time = '34:67:94'
+
+        self.parkomat.curr_time = '12:34:00'
+
+    def test_single_day_leave_time_calculation(self):
+        '''
+        Test if leave time calculations for a single day are correct
+        '''
+        self.parkomat.reset()
+
+        self.parkomat._clock._curr_time = datetime.combine(date(2021, 5, 24), time(12, 34, 00))
+        self.parkomat.put_money(MoneyUnit('2.00'))
+        self.assertEqual(self.parkomat.leave_time, self.parkomat.curr_time + timedelta(minutes=60))
+
+        self.parkomat.put_money(MoneyUnit('2.00'))
+        self.parkomat.put_money(MoneyUnit('2.00'))
+        self.assertEqual(self.parkomat.leave_time, self.parkomat.curr_time + timedelta(minutes=120))
+
+        self.parkomat.put_money(MoneyUnit('5.00'))
+        self.assertEqual(self.parkomat.leave_time, self.parkomat.curr_time + timedelta(minutes=180))
+
+        self.parkomat.put_money(MoneyUnit('5.00'))
+        self.assertEqual(self.parkomat.leave_time, self.parkomat.curr_time + timedelta(minutes=240))
+
+    def test_overnight_leave_time_calculations(self):
+        '''
+        Test if leave time calculations are correct overnight
+        '''
+        self.parkomat.reset()
+        self.parkomat._clock._curr_time = datetime.combine(date(2021, 5, 24), time(17, 20, 00))
+        self.parkomat.put_money(MoneyUnit('2.00'))
+        self.parkomat.put_money(MoneyUnit('2.00'))
+        self.parkomat.put_money(MoneyUnit('2.00'))
+
+        #check if leave time after 19
+        self.assertEqual(self.parkomat.leave_time.hour, 19)
+
+        self.parkomat.put_money(MoneyUnit('5.00'))
+        self.assertEqual(self.parkomat.leave_time, datetime(2021, 5, 25, 8, 20, 00))
+
+    def test_overweekend_leave_time_calculations(self):
+        '''
+        Test if leave time calculations are correct over the weekend
+        '''
+        self.parkomat.reset()
+        self.parkomat._clock._curr_time = datetime.combine(date(2021, 5, 24), time(17, 20, 00))
+
+        self.parkomat.put_money(MoneyUnit('2.00'))
+        self.parkomat.put_money(MoneyUnit('2.00'))
+        self.parkomat.put_money(MoneyUnit('2.00'))
+
+        for _ in range(4):
+            self.parkomat.put_money(MoneyUnit('50.00'))
+            self.parkomat.put_money(MoneyUnit('10.00'))
+
+        #check if leave time on friday
+        self.assertEqual(self.parkomat.leave_time.weekday(), 4)
+
+        self.parkomat.put_money(MoneyUnit('5.00'))
+        self.assertEqual(self.parkomat.leave_time, datetime(2021, 5, 31, 8, 20, 00))
+
+    def test_fractional_leave_time_calculations(self):
+        '''
+        Test if leave time calculations are correct for fractional parts of an hour
+        '''
+        self.parkomat.reset()
+        self.parkomat._clock._curr_time = datetime.combine(date(2021, 5, 24), time(17, 20, 00))
+
+        self.parkomat.put_money(MoneyUnit('1.00'))
+
+        self.assertEqual(self.parkomat.leave_time, datetime(2021, 5, 24, 17, 50, 00))
+
+    def test_leave_time_calculations_with_small_coins(self):
+        '''
+        Test if leave time calculations are correct using small value coins
+        '''
+        self.parkomat.reset()
+        self.parkomat._clock._curr_time = datetime.combine(date(2021, 5, 24), time(17, 20, 00))
+
+        for _ in range(200):
+            self.parkomat.put_money(MoneyUnit('0.01'))
+
+        self.assertEqual(self.parkomat.leave_time, datetime(2021, 5, 24, 18, 20, 00))
+
+    def test_money_storage_full_error(self):
+        '''
+        Test if giving over 200 same coins to MoneyStorage will trigger an error
+        '''
+        self.parkomat.reset()
+        self.parkomat._clock._curr_time = datetime.combine(date(2021, 5, 24), time(17, 20, 00))
+
+        with self.assertRaises(ParkomatFullException):
+            for _ in range(201):
+                self.parkomat.put_money(MoneyUnit('0.01'))
+
+
+    def test_no_coins_given_error(self):
+        '''
+        Test if trying to buy ticket without giving any coins will trigger an error
+        '''
+        self.parkomat.reset()
+        self.parkomat._clock._curr_time = datetime.combine(date(2021, 5, 24), time(17, 20, 00))
+        self.parkomat.registration_number = "KBR1234"
+
+        self.assertRaises(ParkomatNoMoneyInsertedException, self.parkomat.buy)
+
+    def test_incorrect_registration_number_error(self):
+        '''
+        Test if trying to buy ticket without giving a proper registration number will trigger an error
+        '''
+        self.parkomat.reset()
+        self.parkomat._clock._curr_time = datetime.combine(date(2021, 5, 24), time(17, 20, 00))
+        self.parkomat.put_money(MoneyUnit('2.00'))
+
+        self.parkomat.registration_number = ""
+        self.assertRaises(ParkomatEmptyRegistrationNumberException, self.parkomat.buy)
+
+        self.parkomat.registration_number = "dss&&"
+        self.assertRaises(ParkomatIncorrectRegistrationNumberException, self.parkomat.buy)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
